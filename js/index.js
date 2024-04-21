42dc105978ac70d33e9d83854ac035d831f920db
Wizzard2001 <rpossberg1@nait.ca>
2024-04-20 20:14:16 -0600

    chore: Refactor code to use fetcher utility and render albums

--- js/index.js
+++ js/index.js
@@ -1,39 +1,22 @@
-console.log("index.js");
+console.log("index.js"); // log to the console to confirm that the file is being executed
 
-/*
-  Search Album View
-  For the UI view, we need to add the search albums functionality.
-  - search the album data objects artistName and albumName properties.
-  - return any results that match the search query and render the album data using the template provided in the markup. Use interactive templating so the user can add an album to their favorites list.
-
-            <!-- SEARCH  RESULTS  -->
-
-              <!-- TEMPLATE -->
-              <li class="list-group-item d-flex justify-content-between align-items-start">
-                <div class="ms-2 me-auto">
-                  <div class="fw-bold">
-                    Name of the Album
-                    <span class="badge bg-primary rounded-pill">4.23</span>
-                  </div>
-                  <span>Artist</span>
-                </div>
-                <button data-uid=${uid} type="button" class="btn btn-success">Add to Favorites</button>
-              </li>
-
-*/
-import { fetcher } from "./utils/fetcher";
+import { toElement } from "./utils/toElement";
+import { fetcher } from "./utils/fetcher"; // fetcher function
 import { addToFavorites } from "./api/tasks";
 import { albumCard } from "./templates/albumCard";
-import { favoriteCard } from "./templates/favoriteCard.js";
+import { favoriteCard } from "./templates/favoriteCard";
 
 // fetch request to get the albums from the albums resourse on the mock api service
+// store the albums in the store variable
 let store;
+//  get the master copy of the albums container
+const masterCopy = document.querySelector("#favorites").cloneNode(true);
 async function appInit() {
   store = await fetcher(
     "https://66147b812fc47b4cf27c6899.mockapi.io/api/v1/albums"
   );
 
-  renderAlbums(store);
+  renderAlbums(store); // render albums
   console.log(store);
 }
 
@@ -44,6 +27,18 @@ let albumStore = [];
 let favoriteStore = [];
 
 function renderAlbums(albums) {
+  const container = masterCopy.cloneNode(true);
   const albumCardElements = albums.map((albumData) => albumCard(albumData));
-  console.log(object);
+  console.log(albumCardElements);
+
+  albumCardElements.forEach((item) => {
+    container.appendChild(item);
+  });
+
+  // interactive templating
+  //
+  const elem = document.createRange().createContextualFragment(template)
+    .children[0];
+
+  document.querySelector("#favorites").replaceWith(container);
 }
--- js/templates/albumCard.js
+++ js/templates/albumCard.js
@@ -1,13 +1,14 @@
 import { toElement } from "../utils/toElement.js";
 
 function albumTemplate({ artistName, albumName, averageRating, uid }) {
-  const template = `              <li class="list-group-item d-flex justify-content-between align-items-start">
+  const template = `
+  <li data-uid=${uid} class="list-group-item d-flex justify-content-between align-items-start">
     <div class="ms-2 me-auto">
       <div class="fw-bold">
-        Name of the Album
-        <span class="badge bg-primary rounded-pill">4.23</span>
+        ${albumName}
+        <span class="badge bg-primary rounded-pill">${averageRating}</span>
       </div>
-      <span>Artist</span>
+      <span> ${artistName} </span>
     </div>
     <button data-uid=${uid} type="button" class="btn btn-success">Add to Favorites</button>
   </li>`;
@@ -22,5 +23,4 @@ function albumTemplate({ artistName, albumName, averageRating, uid }) {
 export function albumCard(album) {
   const template = albumTemplate(album);
   return toElement(template);
-  console.log(album);
 }
